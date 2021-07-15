f708b080ca07b5230b35cb620ea2c8237862ea1b
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-07-15 16:53:26 +0530

    Some bug fixed and added MQTT connection

--- routes/device.js
+++ routes/device.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const Connection = require("../lib/connection");
+const MQTT = require("../lib/mqtt");
 
 router.get("/:serial/:relay/:message", async (req, res, next) => {
   const { serial, relay, message } = req.params;
   const connection = new Connection();
-  const result = await connection.device(serial, relay, message);
-  res.json(result);
+  const results = await connection.device(serial, relay, message);
+  if (results.success === 1) {
+    const Mqtt = new MQTT();
+    const result = await Mqtt.Publish(serial, relay, message);
+    res.json({ ...result });
+  }
 });
 
 module.exports = router;

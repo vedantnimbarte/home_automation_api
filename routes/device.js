2da5c52c63b5ab1701389a0c736f48aa2986eeac
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-07-24 19:17:19 +0530

    Added id as a parameter to the get route

--- routes/device.js
+++ routes/device.js
@@ -3,13 +3,15 @@ const router = express.Router();
 const Connection = require("../lib/connection");
 const MQTT = require("../lib/mqtt");
 
-router.get("/:serial/:relay/:message", async (req, res, next) => {
-  const { serial, relay, message } = req.params;
+router.get("/:serial/:relay/:message/:id", async (req, res, next) => {
+  const { serial, relay, message, id } = req.params;
+  console.log(serial, relay, message)
   const connection = new Connection();
-  const results = await connection.device(serial, relay, message);
+  const results = await connection.device(serial, relay, message, id);
   if (results.success === 1) {
     const Mqtt = new MQTT();
     const result = await Mqtt.Publish(serial, relay, message);
+
     res.json({ ...result });
   }
 });
